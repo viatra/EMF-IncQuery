41dad641e3b46f2973d358c85a3860b7e90bb5cd
Istvan Rath <rath@mit.bme.hu>
2011-04-17 18:59:55 +0000

    fix enabledness detection for the validation action
    git-svn-id: https://viatra.inf.mit.bme.hu/svn/incquery/trunk@4993 180758aa-af72-4ec8-8b0e-42a8f00b6c8b

--- runtime/org.eclipse.viatra2.emf.incquery.validation.ui/src/org/eclipse/viatra2/emf/incquery/validation/ui/actions/InitializeValidationAction.java
+++ runtime/org.eclipse.viatra2.emf.incquery.validation.ui/src/org/eclipse/viatra2/emf/incquery/validation/ui/actions/InitializeValidationAction.java
@@ -20,7 +20,6 @@ import org.eclipse.viatra2.emf.incquery.validation.core.ValidationUtil;
 
 public class InitializeValidationAction implements IViewActionDelegate, IEditorActionDelegate {
 
-//	private NamedElement selectedElement=null;
 	private Object selectedElement=null;
 
 	
@@ -49,47 +48,19 @@ public class InitializeValidationAction implements IViewActionDelegate, IEditorA
 	 */
 	public void run(IAction action) {
 		this.editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-		// set the color of all elements to default
-//		if (editor instanceof MultiPageEditor) {
-
-//			final TreeOutlinePage outline = (TreeOutlinePage) ((MultiDiagramEditor) editor)
-//					.getAdapter(IContentOutlinePage.class);
-			//outline.setDefault(); // <- re-set colours
-		
-//			final FileEditorInput f = (FileEditorInput) ((MultiPageEditor)editor).getEditorInput();
-			final FileEditorInput f = (FileEditorInput) (editor).getEditorInput();
-			if (selectedElement instanceof EObject) {
-				Resource eResource = ((EObject)selectedElement).eResource();
-				IFile file = f.getFile();
-				
-				try {
-					ValidationUtil.initValidators(eResource, file);
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
+		final FileEditorInput f = (FileEditorInput) (editor).getEditorInput();
+		if (selectedElement instanceof EObject) {
+			Resource eResource = ((EObject)selectedElement).eResource();
+			IFile file = f.getFile();
+			
+			try {
+				ValidationUtil.initValidators(eResource, file);
 			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
-
-	
-//	private Collection<InheritanceDiamondDTO> _processNewMatches(Collection<InheritanceDiamondDTO> dtos, TreeOutlinePage outline, IFile f) throws CoreException {
-//		Vector<InheritanceDiamondDTO> processed = new Vector<InheritanceDiamondDTO>();
-//		for (InheritanceDiamondDTO diamond : dtos) {
-//			System.out.println("New match:"+toStringDTO(diamond));
-//			processed.add(diamond);
-//		}
-//		return processed;
-//	}
-//	
-//	private Collection<InheritanceDiamondDTO> _processLostMatches(Collection<InheritanceDiamondDTO> dtos, TreeOutlinePage outline, IFile f) throws CoreException {
-//		Vector<InheritanceDiamondDTO> processed = new Vector<InheritanceDiamondDTO>();
-//		for (InheritanceDiamondDTO diamond : dtos) {
-//			System.out.println("Lost match:"+toStringDTO(diamond));
-//			processed.add(diamond);
-//		}
-//		return processed;
-//	}
 
 	
 	/**
@@ -97,26 +68,24 @@ public class InitializeValidationAction implements IViewActionDelegate, IEditorA
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
 
-	//	action.setEnabled(false);
+		
+		action.setEnabled(false);
 		//look for selected Element
+		try {
 		Object e=((IStructuredSelection)selection).getFirstElement();
-		this.selectedElement=e;
-		//this is element an elementlink to an UML element?
-//		if (e instanceof EditPart ){
-//			Element element = ((IUMLElementEditPart)e).getUmlElement();
-//			//this is a named Element?
-//			if (element instanceof NamedElement){
-//				action.setEnabled(true);
-//				selectedElement= (NamedElement)element;
-//			}
-////			if (element instanceof org.eclipse.papyrus.umlutils.NamedElement){
-////				action.setEnabled(true);
-////				selectedElement= ((org.eclipse.papyrus.umlutils.NamedElement)element).getUml2NamedElement();
-////			}
-//			
-//		}
+		System.out.println(e);
+		if (e instanceof EObject) {
+			// this action is only compatible with EObjects
+			action.setEnabled(true);
+			this.selectedElement=e;
+			
+		}
+		} catch (Throwable t) {
+			t.printStackTrace(); // TODO this is ugly
+		}
+
 	}
 	public void setActiveEditor(IAction action, IEditorPart targetEditor){
-
+		this.editor = targetEditor;
 	}
 }
