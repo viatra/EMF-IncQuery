e4e0783ef3dee2b88411c1668c3d9e6fb31f09da
Abel Hegedus <hegedusa@mit.bme.hu>
2011-05-27 17:05:10 +0000

    sample validation project generator stub
    git-svn-id: https://viatra.inf.mit.bme.hu/svn/incquery/trunk@5166 180758aa-af72-4ec8-8b0e-42a8f00b6c8b

---
+++ tooling/org.eclipse.viatra2.emf.incquery.validation.codegen/src/org/eclipse/viatra2/emf/incquery/validation/codegen/generator/SampleValidationProjectGenerator.java
@@ -0,0 +1,205 @@
+/*******************************************************************************
+ * Copyright (c) 2004-2010 Akos Horvath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Akos Horvath - initial API and implementation
+ *******************************************************************************/
+
+
+package org.eclipse.viatra2.emf.incquery.validation.codegen.generator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
+import org.eclipse.pde.core.project.IBundleProjectDescription;
+import org.eclipse.pde.core.project.IBundleProjectService;
+import org.eclipse.viatra2.emf.incquery.core.codegen.CodeGenerationException;
+import org.eclipse.viatra2.emf.incquery.core.codegen.util.CodegenSupport;
+import org.eclipse.viatra2.emf.incquery.core.codegen.util.GTPatternJavaData;
+import org.eclipse.viatra2.emf.incquery.core.codegen.util.ModulesLoader;
+import org.eclipse.viatra2.emf.incquery.core.codegen.util.PatternsCollector;
+import org.eclipse.viatra2.emf.incquery.core.project.IncQueryNature;
+import org.eclipse.viatra2.emf.incquery.core.project.SampleProjectSupport;
+import org.eclipse.viatra2.emf.incquery.model.incquerygenmodel.EcoreModel;
+import org.eclipse.viatra2.emf.incquery.model.incquerygenmodel.IncQueryGenmodel;
+import org.eclipse.viatra2.emf.incquery.validation.codegen.ValidationCodegenPlugin;
+import org.eclipse.viatra2.emf.incquery.validation.codegen.internal.ExtensionsforSampleValidationProjectGenerator;
+import org.eclipse.viatra2.emf.incquery.validation.codegen.internal.SampleConstraintGenerator;
+import org.eclipse.viatra2.emf.incquery.validation.codegen.internal.SampleConstraintGenerator.ConstraintData;
+import org.eclipse.viatra2.framework.FrameworkException;
+import org.eclipse.viatra2.framework.FrameworkManagerException;
+import org.eclipse.viatra2.framework.IFramework;
+import org.eclipse.viatra2.gtasmmodel.gtasm.metamodel.gt.GTPattern;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * 
+ * @author Abel Hegedus
+ *
+ */
+public class SampleValidationProjectGenerator {
+	
+	IProject incQueryProject, sampleProject;
+	IncQueryGenmodel iqGen;
+	ModulesLoader modulesLoader;
+	
+	//SampleProjectGenerator sampleProjectGeneartor;
+	
+	public SampleValidationProjectGenerator(IProject incQueryProject,IncQueryGenmodel iqGen, IProgressMonitor monitor) throws CodeGenerationException {
+		super();
+		this.incQueryProject = incQueryProject;
+		this.iqGen = iqGen;
+		this.modulesLoader = new ModulesLoader(incQueryProject);
+	}
+
+	public void fullBuild(IProgressMonitor monitor) throws CodeGenerationException {
+		try {
+			buildProject(monitor);
+			clean(monitor);
+			buildAfterClean(monitor);
+		} catch (OperationCanceledException e) {
+			throw new CodeGenerationException("Error during EMF-IncQuery Sample project generation. ", e);
+		} catch (CoreException e) {
+			throw new CodeGenerationException("Error during EMF-IncQuery Sample project generation. ", e);
+		} catch (FrameworkException e) {
+			throw new CodeGenerationException("Error during EMF-IncQuery Sample project generation. ", e);
+		} catch (FrameworkManagerException e) {
+			throw new CodeGenerationException("Error during EMF-IncQuery Sample project generation. ", e);
+		}
+	}
+
+	private void buildProject(IProgressMonitor monitor) throws OperationCanceledException, CoreException {
+
+		BundleContext context = null;
+		ServiceReference ref = null;
+
+		try {
+			context = ValidationCodegenPlugin.plugin.context;
+			ref = context
+			.getServiceReference(IBundleProjectService.class.getName());
+			IBundleProjectService service = (IBundleProjectService) context
+			.getService(ref);
+			IBundleProjectDescription bundleDesc = service.getDescription(incQueryProject);
+			this.sampleProject = SampleProjectSupport.createProject(monitor, bundleDesc.getBundleName(),incQueryProject.getName());
+		}
+		finally
+		{if(context != null && ref != null)
+			context.ungetService(ref);}
+	}
+
+	public void clean(IProgressMonitor monitor) throws CodeGenerationException {
+		IProject project = getProject();
+		IFolder folder = project.getFolder(IncQueryNature.SRC_DIR);
+		try {
+			folder.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+			for (IResource res : folder.members()) {
+				res.delete(true, monitor);
+			}
+		} catch (CoreException e) {
+			throw new CodeGenerationException("Error during cleanup before code EMF-IncQuery code generation.", e);
+		}
+	}
+
+	public void buildAfterClean(IProgressMonitor monitor) throws CodeGenerationException, FrameworkManagerException, CoreException, FrameworkException {
+			IFramework framework = modulesLoader.loadFramework(incQueryProject);
+			try {
+				modulesLoader.loadAllModules(framework);
+				Set<GTPattern> patterns = new PatternsCollector(framework).getCollectedPatterns();
+				Map<GTPattern, GTPatternJavaData> gtPatternJavaRepresentations = generateGTPatternJavaData(patterns, monitor);
+				SampleConstraintGenerator constraintGenerator = new SampleConstraintGenerator(gtPatternJavaRepresentations, getProject(), incQueryProject);
+				constraintGenerator.generateActivator(sampleProject.getName() , monitor);
+				Map<String,ConstraintData> handlers = constraintGenerator.generateHandlersForPatternMatcherCalls(monitor);
+				ExtensionsforSampleValidationProjectGenerator extensionGenerator = new ExtensionsforSampleValidationProjectGenerator(sampleProject);
+				extensionGenerator.contributeToExtensionPoint(handlers, getFileExtension(iqGen), monitor);
+							
+			} finally {
+				modulesLoader.disposeFramework(framework);
+			}		
+	}
+
+//		
+//		String editorID = incqueryGenmodel.getEcoreModel().get(0).getModels().getEditorPluginID();
+//		String modelName = incqueryGenmodel.getEcoreModel().get(0).getModels().getModelName();
+//		return editorID+"."+modelName+"EditorID";
+//	}
+
+	private Collection<String> getFileExtension(IncQueryGenmodel incqueryGenmodel) {
+		//incqueryGenmodel.getEcoreModel().get(0).getModels().getGenPackages().get(0).getFileExtension();
+
+		// TODO: works only when the editor file extension is an GenPackage's file extension who has at leat one classifiers
+		List<String> fileExtensions = new ArrayList<String>(); 
+		
+		for(EcoreModel ecoreModel :incqueryGenmodel.getEcoreModel())
+			for(GenPackage genPackage: ecoreModel.getModels().getAllGenPackagesWithClassifiers()) 
+			{
+				fileExtensions.add(genPackage.getFileExtension());
+			}
+		return fileExtensions;
+	}
+
+	private Map<GTPattern, GTPatternJavaData> generateGTPatternJavaData(
+			Set<GTPattern> patterns,
+			IProgressMonitor monitor) throws CodeGenerationException {
+		Map<GTPattern, GTPatternJavaData>  datas = new HashMap<GTPattern, GTPatternJavaData>();
+		
+		for(GTPattern pattern: patterns) {
+			GTPatternJavaData data = new GTPatternJavaData();
+			data.setPatternName(pattern.getName());
+			
+			//mathcer
+			IPath pathRoot = incQueryProject.getFolder(IncQueryNature.GENERATED_MATCHERS_DIR).getFullPath();
+			String packageNameRoot = IncQueryNature.GENERATED_MATCHERS_PACKAGEROOT;
+			CodegenSupport.PackageLocationFinder matcherPLF= new CodegenSupport.PackageLocationFinder(pattern, pathRoot, packageNameRoot, monitor);
+			//sets the matcher and the matcher's package
+			data.setMatcherName(getMatcherName(pattern));
+			data.setMatcherPackage(matcherPLF.getJavaPackageName()+"."+getMatcherName(pattern));
+			
+			//singature
+			pathRoot = incQueryProject.getFolder(IncQueryNature.GENERATED_DTO_DIR).getFullPath();
+			packageNameRoot = IncQueryNature.GENERATED_DTO_PACKAGEROOT;
+			CodegenSupport.PackageLocationFinder signaturePLF= new CodegenSupport.PackageLocationFinder(pattern, pathRoot, packageNameRoot, monitor);
+			//sets the matcher and the matcher's package
+			data.setSignatureName(getSignatureName(pattern));
+			data.setSignaturePackage(signaturePLF.getJavaPackageName()+"."+getSignatureName(pattern));
+			
+			datas.put(pattern, data);
+			
+		}
+		return datas;
+	}
+	
+	private String getMatcherName(GTPattern pattern) {
+		String pName = pattern.getName().substring(1);
+		return Character.toUpperCase(pattern.getName().charAt(0))+pName +"Matcher";
+		
+	}
+	
+	private String getSignatureName(GTPattern pattern) {
+		String pName = pattern.getName().substring(1);
+		return Character.toUpperCase(pattern.getName().charAt(0))+pName +"Signature";
+		
+	}
+
+	public IProject getProject() {
+		return sampleProject;
+	}
+
+
+}
---
+++ tooling/org.eclipse.viatra2.emf.incquery.validation.codegen/src/org/eclipse/viatra2/emf/incquery/validation/codegen/internal/SampleConstraintGenerator.java
@@ -0,0 +1,232 @@
+/*******************************************************************************
+ * Copyright (c) 2004-2010 Akos Horvath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Akos Horvath - initial API and implementation
+ *******************************************************************************/
+
+
+package org.eclipse.viatra2.emf.incquery.validation.codegen.internal;
+
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.viatra2.emf.incquery.core.codegen.CodeGenerationException;
+import org.eclipse.viatra2.emf.incquery.core.codegen.util.CodegenSupport;
+import org.eclipse.viatra2.emf.incquery.core.codegen.util.GTPatternJavaData;
+import org.eclipse.viatra2.emf.incquery.core.project.IncQueryNature;
+import org.eclipse.viatra2.gtasm.support.helper.GTASMHelper;
+import org.eclipse.viatra2.gtasmmodel.gtasm.metamodel.gt.GTPattern;
+
+public class SampleConstraintGenerator {
+
+	static final String handlerTemplateFile =
+		"Handler.java.template";
+	static final String activatorTemplateFile =
+		"Activator.java.template";
+	static final String counterTemplateFile =
+		"Counter.java.template";
+
+	final String handlerTemplate,
+		activatorTemplate,counterTemplate;
+
+	IProject sampleProject, incQueryProject;
+
+	Map<GTPattern, GTPatternJavaData> gtPatternJavaRepresentations;
+
+	/**
+	 * @param gtPatternJavaRepresentations
+	 * @param sampleProject
+	 * @param incQueryProject
+	 * @throws CodeGenerationException
+	 */
+	public SampleConstraintGenerator(Map<GTPattern, GTPatternJavaData> gtPatternJavaRepresentations, IProject sampleProject, IProject incQueryProject) throws CodeGenerationException {
+		super();
+		this.sampleProject = sampleProject;
+		this.incQueryProject = incQueryProject;
+		this.gtPatternJavaRepresentations = gtPatternJavaRepresentations;
+
+		this.handlerTemplate = CodegenSupport.loadTemplate(handlerTemplateFile);
+		this.activatorTemplate = CodegenSupport.loadTemplate(activatorTemplateFile);
+		this.counterTemplate = CodegenSupport.loadTemplate(counterTemplateFile);
+	}
+
+
+	public void generateActivator(String projectID, IProgressMonitor monitor) throws CodeGenerationException {
+		Map<String, String> substitutions = new HashMap<String, String>();
+
+		substitutions.put("java-package", IncQueryNature.GENERATED_HANDLER_PACKAGEROOT);
+		substitutions.put("java-project-id", projectID);
+
+		String generatedCode = CodegenSupport.processTemplate(activatorTemplate, substitutions);
+
+
+		try {
+			IFolder rootFolder = sampleProject.getFolder(IncQueryNature.GENERATED_HANDLER_DIR);
+			rootFolder.create(true, true, monitor);
+			//String packageNameRoot = IncQueryNature.GENERATED_HANDLER_PACKAGEROOT;
+
+			IFile file = rootFolder.getFile("Activator.java");
+
+			CodegenSupport.printStringToFile(monitor, generatedCode, file);
+		} catch (UnsupportedEncodingException e) {
+			throw new CodeGenerationException("Error writing generated command handlers to file.", e);
+		} catch (CoreException e) {
+			throw new CodeGenerationException(
+					"Error writing the Activator file of the sample ui project",
+					e);
+		}
+
+	}
+
+	/**Emits generated source code for DSM specific pattern matcher classes for the given patterns.
+	 * @param monitor Progress monitor for the file operations
+	 * @return the source code of handlers in a map
+	 * @throws CodeGenerationException
+	 */
+	public Map<String,ConstraintData> generateHandlersForPatternMatcherCalls(IProgressMonitor monitor) throws CodeGenerationException {
+		Map<String, String> substitutions = new HashMap<String, String>();
+		Map<String,ConstraintData> handlers = new HashMap<String, SampleConstraintGenerator.ConstraintData>();
+		for(Map.Entry<GTPattern, GTPatternJavaData> entry: gtPatternJavaRepresentations.entrySet())
+			{
+			GTPattern pattern = entry.getKey();
+			GTPatternJavaData patternData = entry.getValue();
+
+			substitutions.put("pattern-name", pattern.getName());
+
+			substitutions.put("java-matcher-package", patternData.getMatcherPackage());
+			substitutions.put("java-signature-package", patternData.getSignaturePackage());
+			substitutions.put("java-matcher", patternData.getMatcherName());
+			substitutions.put("java-signature", patternData.getSignatureName());
+
+			String className = getHandlerName(pattern);
+			substitutions.put("java-class-name", className);
+
+			CodegenSupport.PackageLocationFinder packageLocation =
+				getHandlerPackage(pattern, monitor);
+
+			String javaPackageName = packageLocation.getJavaPackageName();
+			substitutions.put("handler-package", javaPackageName);
+
+			
+
+			String generatedCode = null;
+			
+			// process annotation
+			String machinePatternMode = null;
+			Map<String, String> annotation = 
+				GTASMHelper.extractLowerCaseRuntimeAnnotation(pattern, "@GenerationMode");
+			if (annotation != null){
+				machinePatternMode = annotation.get("mode");
+			
+				// counter pattern
+				if ("counter".equals(machinePatternMode)){
+					className = getCounterName(pattern);
+					substitutions.put("java-class-name", className);
+					generatedCode = CodegenSupport.processTemplate(counterTemplate, substitutions);
+				//} //else if("list".equals(machinePatternMode)){
+				 //	generatedCode = CodegenSupport.processTemplate(handlerTemplate, substitutions);
+				} else // if("constraint".equals(machinePatternMode)){
+					continue; // constraints and warnings are handled in validation
+				//}
+			} 
+			// no annotation means default handler
+			if(generatedCode == null){
+				generatedCode = CodegenSupport.processTemplate(handlerTemplate, substitutions);
+			}
+			
+			handlers.put(className, new ConstraintData(javaPackageName, className, pattern.getName()));
+			
+			IFile file = packageLocation.getFolder().getFile(className+ ".java");
+
+			try {
+				CodegenSupport.printStringToFile(monitor, generatedCode, file);
+			} catch (UnsupportedEncodingException e) {
+				throw new CodeGenerationException("Error writing generated command handlers to file.", e);
+			} catch (CoreException e) {
+				throw new CodeGenerationException(
+						"Error writing source code file for generated command handlers of "
+						+ pattern.getFqn(), e);
+			}
+
+			}
+
+		return handlers;
+	}
+
+	/** Returns the name of the Java class representing the pattern's command handler
+	 * @param pattern
+	 * @return
+	 */
+	protected String getHandlerName(GTPattern pattern) {
+		String pName = pattern.getName().substring(1);
+		return Character.toUpperCase(pattern.getName().charAt(0))+pName+"Handler";
+	}
+	
+	/** Returns the name of the Java class representing the pattern's command counter
+	 * @param pattern
+	 * @return
+	 */
+	protected String getCounterName(GTPattern pattern) {
+		String pName = pattern.getName().substring(1);
+		return Character.toUpperCase(pattern.getName().charAt(0))+pName+"Counter";
+	}
+
+	protected CodegenSupport.PackageLocationFinder getHandlerPackage(GTPattern pattern, IProgressMonitor monitor) throws CodeGenerationException {
+		IPath pathRoot = sampleProject.getFolder(IncQueryNature.GENERATED_HANDLER_DIR).getFullPath();
+		String packageNameRoot = IncQueryNature.GENERATED_HANDLER_PACKAGEROOT;
+
+		return
+			new CodegenSupport.PackageLocationFinder(pattern, pathRoot, packageNameRoot, monitor);
+	}
+
+	public static class ConstraintData{
+
+		private String constraintPackage, constraintName, patternName;
+
+		public ConstraintData(String constraintPackage, String constraintName, String patternName) {
+			this.constraintName = constraintName;
+			this.constraintPackage = constraintPackage;
+			this.patternName = patternName;
+		}
+
+		public String getHandlerPackage() {
+			return constraintPackage;
+		}
+
+		public void setHandlerPackage(String handlerPackage) {
+			this.constraintPackage = handlerPackage;
+		}
+
+		public String getHandlerName() {
+			return constraintName;
+		}
+
+		public void setHandlerName(String handlerName) {
+			this.constraintName = handlerName;
+		}
+
+		public String getPatternName() {
+			return patternName;
+		}
+
+		public void setPatternName(String patternName) {
+			this.patternName = patternName;
+		}
+		
+		
+	}
+
+}
