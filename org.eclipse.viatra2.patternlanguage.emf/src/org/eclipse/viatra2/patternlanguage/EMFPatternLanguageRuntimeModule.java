35d09200d3c08bccacdf1ca4e82190f3179e3ea3
ujhelyiz <ujhelyiz@mit.bme.hu>
2011-08-15 14:01:13 +0200

    Refactor: moved EMF-specific language elements to new project

--- org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/EMFPatternLanguageRuntimeModule.java
+++ org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/EMFPatternLanguageRuntimeModule.java
@@ -3,9 +3,31 @@
  */
 package org.eclipse.viatra2.patternlanguage;
 
+import org.eclipse.viatra2.patternlanguage.core.scoping.MyAbstractDeclarativeScopeProvider;
+import org.eclipse.viatra2.patternlanguage.scoping.EMFPatternLanguageDeclarativeScopeProvider;
+import org.eclipse.viatra2.patternlanguage.scoping.EMFPatternLanguageLinkingService;
+import org.eclipse.xtext.linking.ILinkingService;
+import org.eclipse.xtext.scoping.IScopeProvider;
+import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
+import org.eclipse.xtext.xbase.scoping.XbaseImportedNamespaceScopeProvider;
+
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class EMFPatternLanguageRuntimeModule extends org.eclipse.viatra2.patternlanguage.AbstractEMFPatternLanguageRuntimeModule {
 
+	@Override
+	public Class<? extends ILinkingService> bindILinkingService() {
+		return EMFPatternLanguageLinkingService.class;
+	}
+
+	// contributed by org.eclipse.xtext.generator.xbase.XbaseGeneratorFragment
+	@Override
+	public void configureIScopeProviderDelegate(Binder binder) {
+		binder.bind(IScopeProvider.class).annotatedWith(Names.named(AbstractDeclarativeScopeProvider.NAMED_DELEGATE)).to(EMFPatternLanguageDeclarativeScopeProvider.class);
+		binder.bind(IScopeProvider.class).annotatedWith(Names.named(MyAbstractDeclarativeScopeProvider.NAMED_DELEGATE)).to(XbaseImportedNamespaceScopeProvider.class);
+	}
 }
