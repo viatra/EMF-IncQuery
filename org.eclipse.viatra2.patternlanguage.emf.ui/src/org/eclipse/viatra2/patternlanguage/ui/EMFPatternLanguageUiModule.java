e6979a026027058433e8fd7e4058ecf732933e0b
ujhelyiz <ujhelyiz@mit.bme.hu>
2011-10-04 16:25:30 +0200

    Content assist autoactivation support added - further testing needed

--- org.eclipse.viatra2.patternlanguage.emf.ui/src/org/eclipse/viatra2/patternlanguage/ui/EMFPatternLanguageUiModule.java
+++ org.eclipse.viatra2.patternlanguage.emf.ui/src/org/eclipse/viatra2/patternlanguage/ui/EMFPatternLanguageUiModule.java
@@ -4,6 +4,10 @@
 package org.eclipse.viatra2.patternlanguage.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.contentassist.XtextContentAssistProcessor;
+
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -11,5 +15,14 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class EMFPatternLanguageUiModule extends org.eclipse.viatra2.patternlanguage.ui.AbstractEMFPatternLanguageUiModule {
 	public EMFPatternLanguageUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+
+	@Override
+	public void configure(Binder binder) {
+		super.configure(binder);
+		binder.bind(String.class)
+				.annotatedWith(
+						Names.named(XtextContentAssistProcessor.COMPLETION_AUTO_ACTIVATION_CHARS))
+				.toInstance(".,");
 	}
 }
