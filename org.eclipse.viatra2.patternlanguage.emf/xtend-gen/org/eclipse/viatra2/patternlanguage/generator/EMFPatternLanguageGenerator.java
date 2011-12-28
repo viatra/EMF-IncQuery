3841c905e24d98667e5db0dc1c8139c55dea67b5
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2011-12-28 22:41:58 +0100

    Revert "Emptied xtend-gen folders"
    This reverts commit 3d9962ada4fad0126c7abd385de5842501607650.

---
+++ org.eclipse.viatra2.patternlanguage.emf/xtend-gen/org/eclipse/viatra2/patternlanguage/generator/EMFPatternLanguageGenerator.java
@@ -0,0 +1,11 @@
+package org.eclipse.viatra2.patternlanguage.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class EMFPatternLanguageGenerator implements IGenerator {
+  public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+  }
+}
