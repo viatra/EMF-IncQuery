9d1a3b9819f39ca01e99e03debf0c29ede27240b
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-01-11 02:40:46 +0100

    Added copyright headers

--- org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/scoping/EMFPatternLanguageDeclarativeScopeProvider.java
+++ org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/scoping/EMFPatternLanguageDeclarativeScopeProvider.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Zoltan Ujhelyi and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Zoltan Ujhelyi - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.viatra2.patternlanguage.scoping;
 
 import static org.eclipse.emf.ecore.util.EcoreUtil.getRootContainer;
