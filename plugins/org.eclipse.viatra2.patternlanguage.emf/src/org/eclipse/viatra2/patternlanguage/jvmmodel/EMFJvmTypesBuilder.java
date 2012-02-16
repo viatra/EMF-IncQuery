7754513789e7323f209af0a62064eb6fdd44df3e
Mark Czotter <czottermark@gmail.com>
2012-02-16 16:54:12 +0100

    Added some javadoc, comment to the EMFTypesBuilder class.

--- plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/jvmmodel/EMFJvmTypesBuilder.java
+++ plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/jvmmodel/EMFJvmTypesBuilder.java
@@ -6,6 +6,15 @@ import org.eclipse.xtext.common.types.TypesFactory;
 import org.eclipse.xtext.xbase.jvmmodel.JvmTypesBuilder;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
+/**
+ * This subclass is needed for proper jvm model infer, because in Xtext 2.2 the JvmTypesBuilder infers
+ * a constructor with default content ('{}'). This subclass ignores this default stuff. 
+ * In the init procedure you can define the constructor body.
+ * This bug is fixed in the Xtext 2.3 nightly.
+ * @see http://www.eclipse.org/forums/index.php/m/798983/?srch=infer+constructor#msg_798983 
+ * @author mark
+ *
+ */
 @SuppressWarnings("restriction")
 public class EMFJvmTypesBuilder extends JvmTypesBuilder {
 
