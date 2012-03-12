822c1694bee276f958e3819263301460a9a8f481
Tamas Szabo <szabta89@gmail.com>
2012-03-12 15:47:13 +0100

    Signature -> match javadoc update

--- plugins/org.eclipse.viatra2.emf.incquery.databinding.runtime/src/org/eclipse/viatra2/emf/incquery/databinding/runtime/DatabindingAdapter.java
+++ plugins/org.eclipse.viatra2.emf.incquery.databinding.runtime/src/org/eclipse/viatra2/emf/incquery/databinding/runtime/DatabindingAdapter.java
@@ -4,27 +4,27 @@ import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.viatra2.emf.incquery.runtime.api.IPatternMatch;
 
 /**
- * The class is used to observ given parameters of a match set.
+ * The class is used to observ given parameters of a pattern.
  * 
  * @author Tamas Szabo
  *
- * @param <T> the type parameter of the signature
+ * @param <T> the type parameter of the match
  */
 public abstract class DatabindingAdapter<T extends IPatternMatch> {
 	
 	/**
-	 * Returns the array of observable valuess.
+	 * Returns the array of observable values.
 	 * 
 	 * @return the array of values
 	 */
 	public abstract String[] getParameterNames();
 	
 	/**
-	 * Returns an observable value for the given signature and parameterName.
+	 * Returns an observable value for the given match and parameterName.
 	 * 
-	 * @param signature the signature
+	 * @param match the match object
 	 * @param parameterName the parameter name
 	 * @return an observable value
 	 */
-	public abstract IObservableValue getObservableParameter(T signature, String parameterName);
+	public abstract IObservableValue getObservableParameter(T match, String parameterName);
 }
