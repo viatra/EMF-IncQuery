6323d6d2f0248a283e861ed1454bf55212478a9e
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-03-24 18:21:04 +0100

    #6: Xtext 2.3 compatibility changes

--- plugins/org.eclipse.viatra2.emf.incquery.tooling.generator/src/org/eclipse/viatra2/emf/incquery/tooling/generator/util/EMFJvmTypesBuilder.java
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.generator/src/org/eclipse/viatra2/emf/incquery/tooling/generator/util/EMFJvmTypesBuilder.java
@@ -1,13 +1,10 @@
 package org.eclipse.viatra2.emf.incquery.tooling.generator.util;
 
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.xtext.common.types.JvmConstructor;
 import org.eclipse.xtext.common.types.JvmLowerBound;
 import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.common.types.JvmWildcardTypeReference;
 import org.eclipse.xtext.common.types.TypesFactory;
 import org.eclipse.xtext.xbase.jvmmodel.JvmTypesBuilder;
-import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
 import com.google.inject.Inject;
 
@@ -22,25 +19,6 @@ public class EMFJvmTypesBuilder extends JvmTypesBuilder {
 
 	@Inject
 	private TypesFactory factory = TypesFactory.eINSTANCE;
-	
-	/**
-	 * This overriden method is needed for proper constructor inference. In
-	 * Xtext 2.2 the {@link JvmTypesBuilder} infers constructor with default
-	 * content <code>('{}')</code>. This method ignores this default stuff. This bug is fixed
-	 * in the Xtext 2.3 nightly.
-	 * In the init {@link Procedure1} you can define the constructor body.
-	 * @see <a
-	 *      href="http://www.eclipse.org/forums/index.php/m/798983/?srch=infer+constructor#msg_798983">Eclipse
-	 *      Forum Message</a>
-	 * @return {@link JvmConstructor}
-	 */
-   	public JvmConstructor toConstructor(EObject sourceElement, String simpleName, Procedure1<JvmConstructor> init) {
-   		JvmConstructor constructor = TypesFactory.eINSTANCE.createJvmConstructor();
-		constructor.setSimpleName(nullSaveName(simpleName));
-		if (init != null && simpleName != null)
-			init.apply(constructor);
-		return associate(sourceElement, constructor);
-	}
    	
 	/**
 	 * Creates a {@link JvmWildcardTypeReference} with a {@link JvmLowerBound}
