32ce1cf636e268f5e0369090161ea2c5ba8318a9
Tamas Szabo <szabta89@gmail.com>
2012-04-01 12:11:19 +0200

    added definition for eobject #45 #46

--- plugins/org.eclipse.viatra2.emf.incquery.validation.runtime.ui/src/org/eclipse/viatra2/emf/incquery/validation/runtime/ui/handlers/InitValidatorsHandler.java
+++ plugins/org.eclipse.viatra2.emf.incquery.validation.runtime.ui/src/org/eclipse/viatra2/emf/incquery/validation/runtime/ui/handlers/InitValidatorsHandler.java
@@ -3,11 +3,24 @@ package org.eclipse.viatra2.emf.incquery.validation.runtime.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class InitValidatorsHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IEditorPart activeEditor = HandlerUtil.getActiveEditor(event);
+		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelection(event);
+		
+		Object selectedElement = selection.getFirstElement();
+		
+		if (selectedElement instanceof EObject) {
+			System.out.println();
+		}
+		
 		System.out.println("initValidators");
 		return null;
 	}
