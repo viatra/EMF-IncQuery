0c02f39a62825d6016eb19b2114d76aae1710375
Tamas Szabo <szabta89@gmail.com>
2012-04-01 11:37:04 +0200

    ported validation framework contributions to command framework #45 #46

---
+++ plugins/org.eclipse.viatra2.emf.incquery.validation.runtime.ui/src/org/eclipse/viatra2/emf/incquery/validation/runtime/ui/handlers/InitValidatorsHandler.java
@@ -0,0 +1,14 @@
+package org.eclipse.viatra2.emf.incquery.validation.runtime.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class InitValidatorsHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("initValidators");
+		return null;
+	}
+}
