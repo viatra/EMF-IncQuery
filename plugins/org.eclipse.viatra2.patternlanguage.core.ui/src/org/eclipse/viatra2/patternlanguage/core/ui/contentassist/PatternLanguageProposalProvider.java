9258a38f70a76a0940b696f83fd7e8e4a1d4f51f
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-05-06 20:31:38 +0200

    Extending content assist #62  Annotation names

--- plugins/org.eclipse.viatra2.patternlanguage.core.ui/src/org/eclipse/viatra2/patternlanguage/core/ui/contentassist/PatternLanguageProposalProvider.java
+++ plugins/org.eclipse.viatra2.patternlanguage.core.ui/src/org/eclipse/viatra2/patternlanguage/core/ui/contentassist/PatternLanguageProposalProvider.java
@@ -10,9 +10,29 @@
  *******************************************************************************/
 package org.eclipse.viatra2.patternlanguage.core.ui.contentassist;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.viatra2.patternlanguage.core.annotations.PatternAnnotationProvider;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
+import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
+
+import com.google.inject.Inject;
+
 /**
  * see http://www.eclipse.org/Xtext/documentation/latest/xtext.html#contentAssist on how to customize content assistant
  */
 public class PatternLanguageProposalProvider extends AbstractPatternLanguageProposalProvider {
 
+	@Inject
+	PatternAnnotationProvider annotationProvider;
+	
+	public void complete_Annotation(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+		for (String annotationName : annotationProvider.getAllAnnotationNames()) {
+			String prefixedName = String.format("@%s", annotationName);
+			acceptor.accept(createCompletionProposal(prefixedName, prefixedName, null, context));
+		}
+	}
+	public void complete_AnnotationParameter(EObject model, RuleCall ruleCall, ContentAssistContext context, ICompletionProposalAcceptor acceptor) {
+		// subclasses may override
+	}
 }
