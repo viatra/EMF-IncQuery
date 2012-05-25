6bc48607c1bc484b600e013c5afd5480cacb5dbb
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-05-26 01:23:08 +0200

    Refactored existing loaders into different handlers #153

--- plugins/org.eclipse.viatra2.emf.incquery.tooling.gui/src/org/eclipse/viatra2/emf/incquery/queryexplorer/handlers/LoadModelHandler.java
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.gui/src/org/eclipse/viatra2/emf/incquery/queryexplorer/handlers/LoadModelHandler.java
@@ -3,50 +3,37 @@ package org.eclipse.viatra2.emf.incquery.queryexplorer.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.edit.domain.IEditingDomainProvider;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.viatra2.emf.incquery.queryexplorer.QueryExplorer;
-import org.eclipse.xtext.xbase.ui.editor.XbaseEditor;
 
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-
-@SuppressWarnings("restriction")
 public class LoadModelHandler extends AbstractHandler {
 
-	@Inject
-	Injector injector;
-	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		try {
-			IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
-			
-			if (editorPart instanceof IEditingDomainProvider) {
-				IEditingDomainProvider providerEditor = (IEditingDomainProvider) editorPart;
-				
-				ResourceSet resourceSet = providerEditor.getEditingDomain().getResourceSet();
-				if (resourceSet.getResources().size() > 0) {
-					HandlerUtil.getActivePart(event).getSite().getPage().addPartListener(QueryExplorer.getInstance().getModelPartListener());
-					QueryExplorer.getInstance().getMatcherTreeViewerRoot().addPatternMatcherRoot(editorPart, resourceSet);
-				}
+
+		IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+
+		if (editorPart instanceof IEditingDomainProvider) {
+			IEditingDomainProvider providerEditor = (IEditingDomainProvider) editorPart;
+
+			ResourceSet resourceSet = providerEditor.getEditingDomain()
+					.getResourceSet();
+			if (resourceSet.getResources().size() > 0) {
+				HandlerUtil
+						.getActivePart(event)
+						.getSite()
+						.getPage()
+						.addPartListener(
+								QueryExplorer.getInstance()
+										.getModelPartListener());
+				QueryExplorer.getInstance().getMatcherTreeViewerRoot()
+						.addPatternMatcherRoot(editorPart, resourceSet);
 			}
-			else if (editorPart instanceof XbaseEditor) {
-				IFile file = (IFile) HandlerUtil.getActiveEditorInput(event).getAdapter(IFile.class);	
-				if (file != null) {
-					RuntimeMatcherRegistrator registrator = new RuntimeMatcherRegistrator(file);
-					injector.injectMembers(registrator);
-					registrator.run();
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-				
+
 		return null;
 	}
 }
---
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.gui/src/org/eclipse/viatra2/emf/incquery/queryexplorer/handlers/RegisterPatternFileHandler.java
@@ -0,0 +1,37 @@
+package org.eclipse.viatra2.emf.incquery.queryexplorer.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+public class RegisterPatternFileHandler extends AbstractHandler {
+
+	@Inject
+	Injector injector;
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		try {
+			IFile file = (IFile) HandlerUtil.getActiveEditorInput(event)
+					.getAdapter(IFile.class);
+			if (file != null) {
+				RuntimeMatcherRegistrator registrator = new RuntimeMatcherRegistrator(
+						file);
+				injector.injectMembers(registrator);
+				registrator.run();
+			}
+		} catch (Exception e) {
+			throw new ExecutionException("Cannot load pattern file", e);
+		}
+
+		return null;
+	}
+}
