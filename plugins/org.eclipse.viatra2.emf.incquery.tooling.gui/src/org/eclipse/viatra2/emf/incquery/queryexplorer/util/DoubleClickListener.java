077e0d8b9ad9888f58adfe5966041f4d609b5745
Abel Hegedus <hegedusa@mit.bme.hu>
2012-05-26 21:33:56 +0200

    added fix for double-click #154

--- plugins/org.eclipse.viatra2.emf.incquery.tooling.gui/src/org/eclipse/viatra2/emf/incquery/queryexplorer/util/DoubleClickListener.java
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.gui/src/org/eclipse/viatra2/emf/incquery/queryexplorer/util/DoubleClickListener.java
@@ -1,10 +1,15 @@
 package org.eclipse.viatra2.emf.incquery.queryexplorer.util;
 
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.NotEnabledException;
+import org.eclipse.core.commands.NotHandledException;
+import org.eclipse.core.commands.common.NotDefinedException;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.viatra2.emf.incquery.queryexplorer.handlers.ShowLocationHandler;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.IHandlerService;
 
 public class DoubleClickListener implements IDoubleClickListener {
 
@@ -13,7 +18,23 @@ public class DoubleClickListener implements IDoubleClickListener {
 		ISelection selection = event.getSelection();
 		if (selection != null && selection instanceof TreeSelection) {
 			// FIXME how to invoke GMF?
-			new ShowLocationHandler().showLocation((TreeSelection) selection);
+			IHandlerService handlerService = (IHandlerService) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService(IHandlerService.class);
+			try {
+				handlerService.executeCommand(CommandConstants.SHOW_LOCATION_COMMAND_ID, null);
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NotDefinedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NotEnabledException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NotHandledException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//new ShowLocationHandler().showLocation((TreeSelection) selection);
 		}
 	}
 
