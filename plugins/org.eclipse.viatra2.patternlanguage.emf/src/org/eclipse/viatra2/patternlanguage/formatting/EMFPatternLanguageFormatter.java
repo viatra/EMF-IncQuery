a8d5d0cbf8ebc6b9a89244ad9dc08e032d47690c
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-05-30 01:54:07 +0200

    Reworked pretty printer, new formatting rules added #157

--- plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/formatting/EMFPatternLanguageFormatter.java
+++ plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/formatting/EMFPatternLanguageFormatter.java
@@ -26,12 +26,31 @@ public class EMFPatternLanguageFormatter extends AbstractDeclarativeFormatter {
 	
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
-		// Preserve newlines around comments
 		EMFPatternLanguageGrammarAccess grammar = (EMFPatternLanguageGrammarAccess) getGrammarAccess();
+		
+		EMFPatternModelElements patternModelAccess = grammar.getEMFPatternModelAccess();
+		c.setLinewrap(2).after(patternModelAccess.getPackageNameAssignment_1_1());
+		c.setLinewrap(1).after(patternModelAccess.getImportPackagesAssignment_2());
+		c.setLinewrap().before(patternModelAccess.getPatternsAssignment_3());
+		c.setLinewrap(2).before(patternModelAccess.getPatternsAssignment_3());
+		c.setLinewrap(2).between(patternModelAccess.getPatternsAssignment_3(), patternModelAccess.getPatternsAssignment_3());
+		
+		PatternElements patternAccess = grammar.getPatternAccess();
+		c.setLinewrap(1).after(patternAccess.getAnnotationsAssignment_0());
+		c.setSpace(" ").around(patternAccess.getOrKeyword_9_0());
+		
+		AnnotationElements annotationAccess = grammar.getAnnotationAccess();
+		c.setLinewrap().after(annotationAccess.getRule());
+		
+		// Preserve newlines around comments
 		c.setLinewrap(0, 1, 2).before(grammar.getSL_COMMENTRule());
 		c.setLinewrap(0, 1, 2).before(grammar.getML_COMMENTRule());
 		c.setLinewrap(0, 1, 1).after(grammar.getML_COMMENTRule());
 		
+		
+		for (Keyword keyword : grammar.findKeywords("=")) {
+			c.setSpace(" ").around(keyword);
+		}
 		for (Keyword keyword : grammar.findKeywords(".")) {
 			c.setNoSpace().before(keyword);
 			c.setNoSpace().after(keyword);
@@ -52,33 +71,27 @@ public class EMFPatternLanguageFormatter extends AbstractDeclarativeFormatter {
 			c.setNoSpace().before(keyword);
 			c.setNoSpace().after(keyword);
 		}
-		AnnotationElements annotationAccess = grammar.getAnnotationAccess();
-		for (Keyword keyword : grammar.findKeywords(")")) { 
-			if (!keyword.equals(annotationAccess.getRightParenthesisKeyword_2_3())) {
-				c.setNoSpace().before(keyword);
-				c.setNoSpace().after(keyword);
-			} else {
-				c.setLinewrap(1).after(keyword);
-			}
+		for (Keyword keyword : grammar.findKeywords(";")) {
+			c.setNoSpace().before(keyword);
+			c.setLinewrap(1,1,2).after(keyword);
+		}
+		
+		for (Keyword keyword : grammar.findKeywords(")")) {
+			c.setNoSpace().before(keyword);
+			c.setLinewrap(1).after(keyword);
 		}
 		for (Keyword keyword : grammar.findKeywords("@")) {
 			c.setNoSpace().after(keyword);
 		}
-		
-		EMFPatternModelElements patternModelAccess = grammar.getEMFPatternModelAccess();
-		c.setLinewrap(2).after(patternModelAccess.getPackageNameAssignment_1_1());
-		c.setLinewrap(1).after(patternModelAccess.getImportPackagesAssignment_2());
-		c.setLinewrap().before(patternModelAccess.getPatternsAssignment_3());
-		
-		
-		PatternElements patternAccess = grammar.getPatternAccess();
-		c.setLinewrap(1).after(patternAccess.getAnnotationsAssignment_0());
-		
-		PatternBodyElements patternBodyAccess = grammar.getPatternBodyAccess();
-		c.setLinewrap(1, 1, 2).before(patternBodyAccess.getConstraintsAssignment_3_0());
-		c.setLinewrap(1, 1, 2).before(patternBodyAccess.getRightCurlyBracketKeyword_4());
-		c.setLinewrap(2).after(patternBodyAccess.getRightCurlyBracketKeyword_4());
-		c.setIndentationIncrement().after(patternBodyAccess.getLeftCurlyBracketKeyword_2());
-		c.setIndentationDecrement().before(patternBodyAccess.getRightCurlyBracketKeyword_4());
+
+		for (Keyword keyword : grammar.findKeywords("{")) {
+			c.setSpace(" ").before(keyword);
+			c.setLinewrap(1,1,2).after(keyword);
+			c.setIndentationIncrement().after(keyword);
+		}
+		for (Keyword keyword : grammar.findKeywords("}")) {
+			c.setLinewrap(2).after(keyword);
+			c.setIndentationDecrement().before(keyword);
+		}
 	}
 }
