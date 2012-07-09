cb10378b5d2a01e7c60232afab8ebd3a12555437
Abel Hegedus <hegedusa@mit.bme.hu>
2012-07-09 11:22:53 +0200

    corrected wellbehaving extension point id mess

--- plugins/org.eclipse.viatra2.emf.incquery.tooling.core/src/org/eclipse/viatra2/emf/incquery/core/project/ProjectGenerationHelper.java
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.core/src/org/eclipse/viatra2/emf/incquery/core/project/ProjectGenerationHelper.java
@@ -27,6 +27,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.pde.core.plugin.IExtensions;
