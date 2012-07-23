viatra/EMF-IncQuery

commit a7c66b5a847364280387e28b0e62425924aa0fed

Replacing variableRef.var references with variableRef.variable.name #250   variableRef.var does not distinguish between anonymous variables

plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/types/EMFPatternTypeProvider.java
@@ -330,7 +330,7 @@ public class EMFPatternTypeProvider extends XbaseTypeProvider {
 			return false;
 		}
 		final Variable variableReferenceVariable = variableReference.getVariable();
-		final String variableName = variableReference.getVar();
+		final String variableName = variableReference.getVariable().getName();
 		if (equal(variable, variableReferenceVariable) || equal(variableName, variable.getName())) {
 			return true;
 		}

plugins/org.eclipse.viatra2.emf.incquery.runtime/src/org/eclipse/viatra2/emf/incquery/runtime/internal/matcherbuilder/EPMBodyToPSystem.java
@@ -130,7 +130,9 @@ public class EPMBodyToPSystem<StubHandle, Collector> {
 		return getPNode(variable.getName());
 	}
 	protected PVariable getPNode(VariableReference variable) {
-		return getPNode(variable.getVar());
+		// Warning! variable.getVar() does not differentiate between 
+		// multiple anonymous variables ('_')
+		return getPNode(variable.getVariable().getName());
 	}
 	protected Tuple getPNodeTuple(List<? extends ValueReference> variables) throws RetePatternBuildException {
 		PVariable[] pNodeArray = getPNodeArray(variables);

commit c0084eb507da90ea9f6cff1e74dbc111fc9b8a89

Fixed breaking code generator regressions after a7c66b5a84 and 7f79fa49a5

plugins/org.eclipse.viatra2.emf.incquery.tooling.generator/src/org/eclipse/viatra2/emf/incquery/tooling/generator/jvmmodel/EMFPatternLanguageJvmModelInferrer.xtend
@@ -123,6 +123,9 @@ class EMFPatternLanguageJvmModelInferrer extends AbstractModelInferrer {
    			val groupClass = model.inferPatternGroup
    			model.associatePrimary(groupClass)
    			acceptor.accept(groupClass)
+   			for (pattern : model.patterns){
+   				pattern.infer(acceptor, isPrelinkingPhase)
+   			}
    		} catch (IllegalArgumentException e){
    			errorFeedback.reportErrorNoLocation(model, e.message, GeneratorIssueCodes::INVALID_PATTERN_MODEL_CODE, Severity::ERROR, IErrorFeedback::JVMINFERENCE_ERROR_TYPE)
    		} catch(Exception e) {

plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/types/EMFPatternTypeProvider.java
@@ -175,6 +175,7 @@ public class EMFPatternTypeProvider extends XbaseTypeProvider {
 	 * @return
 	 */
 	protected Type searchForConstraintType(PatternBody body, Variable variable) {
+		body.getVariables();
 		for (Constraint constraint : body.getConstraints()) {
 			if (constraint instanceof EClassifierConstraint) {
 				if (equalVariable(variable, ((EClassifierConstraint) constraint).getVar())) {