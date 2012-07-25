9a826b5b368407d1c87ced2da9800584def579b7
Abel Hegedus <hegedusa@mit.bme.hu>
2012-07-25 15:50:14 +0200

    minor refactorings caused by changes in the factory registry

--- plugins/org.eclipse.viatra2.emf.incquery.tooling.gui/src/org/eclipse/viatra2/emf/incquery/queryexplorer/util/DatabindingUtil.java
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.gui/src/org/eclipse/viatra2/emf/incquery/queryexplorer/util/DatabindingUtil.java
@@ -75,7 +75,7 @@ public class DatabindingUtil {
 	private static ILog logger = IncQueryGUIPlugin.getDefault().getLog(); 
 	private static Map<String, IMarker> orderByPatternMarkers = new HashMap<String, IMarker>();
 	private static List<Pattern> generatedPatterns;
-	private static Map<Pattern, IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>>> generatedMatcherFactories;
+	private static Map<Pattern, IMatcherFactory<?>> generatedMatcherFactories;
 	public static final String QUERY_EXPLORER_ANNOTATION = "QueryExplorer";
 	public static final String PATTERNUI_ANNOTATION = "PatternUI";
 	public static final String ORDERBY_ANNOTATION = "OrderBy";
@@ -111,9 +111,9 @@ public class DatabindingUtil {
 		}
 	}
 	
-	private static Map<Pattern, IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>>> collectGeneratedMatcherFactories() {
-		Map<Pattern, IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>>> factories = new HashMap<Pattern, IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>>>();
-		for (IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>> factory : MatcherFactoryRegistry.getContributedMatcherFactories()) {
+	private static Map<Pattern, IMatcherFactory<?>> collectGeneratedMatcherFactories() {
+		Map<Pattern, IMatcherFactory<?>> factories = new HashMap<Pattern, IMatcherFactory<?>>();
+		for (IMatcherFactory<?> factory : MatcherFactoryRegistry.getContributedMatcherFactories()) {
 			Pattern pattern = factory.getPattern();
 			Boolean annotationValue = getValueOfQueryExplorerAnnotation(pattern);
 			if (annotationValue != null && annotationValue) {
@@ -138,7 +138,7 @@ public class DatabindingUtil {
 		}
 	}
 	
-	public static synchronized Collection<IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>>> getGeneratedMatcherFactories() {
+	public static synchronized Collection<IMatcherFactory<?>> getGeneratedMatcherFactories() {
 		if (generatedMatcherFactories == null) {
 			generatedMatcherFactories = collectGeneratedMatcherFactories();
 		}
@@ -154,7 +154,7 @@ public class DatabindingUtil {
 	
 	private static List<Pattern> collectGeneratedPatterns() {
 		List<Pattern> patterns = new ArrayList<Pattern>();
-		for (IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>> factory : getGeneratedMatcherFactories()) {
+		for (IMatcherFactory<?> factory : getGeneratedMatcherFactories()) {
 			patterns.add(factory.getPattern());
 		}
 		return patterns;
@@ -459,7 +459,7 @@ public class DatabindingUtil {
 	 * @param pattern the pattern instance
 	 * @return the matcher factory for the given pattern
 	 */
-	public static IMatcherFactory<IncQueryMatcher<? extends IPatternMatch>> getMatcherFactoryForGeneratedPattern(Pattern pattern) {
+	public static IMatcherFactory<?> getMatcherFactoryForGeneratedPattern(Pattern pattern) {
 		return generatedMatcherFactories.get(pattern);
 	}
 
