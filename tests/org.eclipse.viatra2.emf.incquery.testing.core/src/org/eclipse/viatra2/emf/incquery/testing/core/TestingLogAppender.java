37ac7065f1b9281ecb7b48f7606b5ed970ada166
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-07-26 22:39:23 +0200

    Organized imports in test projects #197

--- tests/org.eclipse.viatra2.emf.incquery.testing.core/src/org/eclipse/viatra2/emf/incquery/testing/core/TestingLogAppender.java
+++ tests/org.eclipse.viatra2.emf.incquery.testing.core/src/org/eclipse/viatra2/emf/incquery/testing/core/TestingLogAppender.java
@@ -10,18 +10,13 @@
  *******************************************************************************/
 package org.eclipse.viatra2.emf.incquery.testing.core;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.log4j.Appender;
 import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Layout;
 import org.apache.log4j.Level;
 import org.apache.log4j.PatternLayout;
-import org.apache.log4j.spi.Filter;
 import org.apache.log4j.spi.LoggingEvent;
 import org.apache.log4j.spi.ThrowableInformation;
 import org.apache.log4j.varia.LevelRangeFilter;
