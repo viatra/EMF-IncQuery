f7a4ba6d071229dbdb871bbafa0a8a0335b40eab
Oszkár Semeráth <graybonedancer@gmail.com>
2012-08-27 17:40:30 +0200

    I've made resolution method public.

--- plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/types/EMFPatternTypeProvider.java
+++ plugins/org.eclipse.viatra2.patternlanguage.emf/src/org/eclipse/viatra2/patternlanguage/types/EMFPatternTypeProvider.java
@@ -96,7 +96,7 @@ public class EMFPatternTypeProvider extends XbaseTypeProvider {
 	 * @param variable
 	 * @return
 	 */
-	protected JvmTypeReference resolve(Variable variable) {
+	public JvmTypeReference resolve(Variable variable) {
 		EcoreUtil2.resolveAll(variable);
 		JvmTypeReference typeRef = resolve(variable, variable);
 		if (typeRef == null) {
@@ -158,7 +158,7 @@ public class EMFPatternTypeProvider extends XbaseTypeProvider {
 	 * @param variable
 	 * @return
 	 */
-	protected JvmTypeReference resolve(PatternBody body, Variable variable) {
+	public JvmTypeReference resolve(PatternBody body, Variable variable) {
 		final Type constraintType = searchForConstraintType(body, variable);
 		if (constraintType != null) {
 			return resolve(constraintType, variable);			
