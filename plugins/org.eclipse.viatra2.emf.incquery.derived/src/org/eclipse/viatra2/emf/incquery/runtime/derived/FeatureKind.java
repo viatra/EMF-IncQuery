1d5917999b5b11dbae5bde5ef152798ddbecb97d
Istvan Rath <istvan.rath@gmail.com>
2012-09-20 16:00:16 +0200

    fix "Dustman" :)

--- plugins/org.eclipse.viatra2.emf.incquery.derived/src/org/eclipse/viatra2/emf/incquery/runtime/derived/FeatureKind.java
+++ plugins/org.eclipse.viatra2.emf.incquery.derived/src/org/eclipse/viatra2/emf/incquery/runtime/derived/FeatureKind.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2010-2012, Dustman, Istvan Rath and Daniel Varro
+ * Copyright (c) 2010-2012, Abel Hegedus, Istvan Rath and Daniel Varro
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Dustman - initial API and implementation
+ *   Abel Hegedus - initial API and implementation
  *******************************************************************************/
 package org.eclipse.viatra2.emf.incquery.runtime.derived;
 
