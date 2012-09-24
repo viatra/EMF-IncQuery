8f46db8ac76059e42b5e1103f13079b5aad88275
Abel Hegedus <hegedusa@mit.bme.hu>
2012-09-24 13:54:44 +0200

    fixed #299 catch IllegalArgumentException when Uri is not valid

--- plugins/org.eclipse.viatra2.emf.incquery.tooling.generator.model/src/org/eclipse/viatra2/emf/incquery/tooling/generator/scoping/GeneratorModelLinkingService.java
+++ plugins/org.eclipse.viatra2.emf.incquery.tooling.generator.model/src/org/eclipse/viatra2/emf/incquery/tooling/generator/scoping/GeneratorModelLinkingService.java
@@ -31,60 +31,64 @@ import org.eclipse.xtext.nodemodel.INode;
 import com.google.inject.Inject;
 
 public class GeneratorModelLinkingService extends DefaultLinkingService {
-	private static final Logger LOG = Logger.getLogger(GeneratorModelLinkingService.class);
-	
-	@Inject
-	private IValueConverterService valueConverterService;
-	
-	@Override
-	public List<EObject> getLinkedObjects(EObject context, EReference ref, INode node) {
-		if (ref == GeneratorModelPackage.eINSTANCE
-				.getGeneratorModelReference_Genmodel()
-				&& context instanceof GeneratorModelReference
-				&& node instanceof ILeafNode) {
-			return getGenModel((GeneratorModelReference) context, (ILeafNode) node);
-		} 
-		return super.getLinkedObjects(context, ref, node);
-	}
-	
-	private List<EObject> getGenModel(GeneratorModelReference context, ILeafNode text) {
-		String nsUri = getMetamodelNsURI(text);
-		if (nsUri == null) {
-			return Collections.emptyList();
-		}
-		GenModel pack = loadGenmodel(nsUri, context.eResource().getResourceSet());
-		if (pack != null) {
-			return Collections.<EObject>singletonList(pack);
-		}
-		return Collections.emptyList();
-	}
+    private static final Logger LOG = Logger.getLogger(GeneratorModelLinkingService.class);
+    
+    @Inject
+    private IValueConverterService valueConverterService;
+    
+    @Override
+    public List<EObject> getLinkedObjects(EObject context, EReference ref, INode node) {
+        if (ref == GeneratorModelPackage.eINSTANCE
+                .getGeneratorModelReference_Genmodel()
+                && context instanceof GeneratorModelReference
+                && node instanceof ILeafNode) {
+            return getGenModel((GeneratorModelReference) context, (ILeafNode) node);
+        } 
+        return super.getLinkedObjects(context, ref, node);
+    }
+    
+    private List<EObject> getGenModel(GeneratorModelReference context, ILeafNode text) {
+        String nsUri = getMetamodelNsURI(text);
+        if (nsUri == null) {
+            return Collections.emptyList();
+        }
+        GenModel pack = loadGenmodel(nsUri, context.eResource().getResourceSet());
+        if (pack != null) {
+            return Collections.<EObject>singletonList(pack);
+        }
+        return Collections.emptyList();
+    }
 
-	private String getMetamodelNsURI(ILeafNode text) {
-		try {
-			return (String) valueConverterService.toValue(text.getText(), getLinkingHelper().getRuleNameFrom(text
-					.getGrammarElement()), text);
-		} catch (ValueConverterException e) {
-			LOG.debug("Exception on leaf '" + text.getText() + "'", e);
-			return null;
-		}
-	}
-	
-	private GenModel loadGenmodel(String resourceOrNsURI, ResourceSet resourceSet) {
-		URI uri = URI.createURI(resourceOrNsURI);
-		try {
-			if (uri.fragment() == null) {
-				Resource resource = resourceSet.getResource(uri, true);
-				return (GenModel) resource.getContents().get(0);
-			}
-			return (GenModel) resourceSet.getEObject(uri, true);
-		} catch(RuntimeException ex) {
-			if (uri.isPlatformResource()) {
-				String platformString = uri.toPlatformString(true);
-				URI platformPluginURI = URI.createPlatformPluginURI(platformString, true);
-				return loadGenmodel(platformPluginURI.toString(), resourceSet);
-			}
-			LOG.trace("Cannot load package with URI '" + resourceOrNsURI + "'", ex);
-			return null;
-		}
-	}
+    private String getMetamodelNsURI(ILeafNode text) {
+        try {
+            return (String) valueConverterService.toValue(text.getText(), getLinkingHelper().getRuleNameFrom(text
+                    .getGrammarElement()), text);
+        } catch (ValueConverterException e) {
+            LOG.debug("Exception on leaf '" + text.getText() + "'", e);
+            return null;
+        }
+    }
+    
+    private GenModel loadGenmodel(String resourceOrNsURI, ResourceSet resourceSet) {
+        URI uri = null;
+        try {
+            uri = URI.createURI(resourceOrNsURI);
+            if (uri.fragment() == null) {
+                Resource resource = resourceSet.getResource(uri, true);
+                return (GenModel) resource.getContents().get(0);
+            }
+            return (GenModel) resourceSet.getEObject(uri, true);
+        } catch (IllegalArgumentException ex) {
+            LOG.trace("Invalid package URI: '" + resourceOrNsURI + "'", ex);
+            return null;
+        } catch(RuntimeException ex) {
+            if (uri != null && uri.isPlatformResource()) {
+                String platformString = uri.toPlatformString(true);
+                URI platformPluginURI = URI.createPlatformPluginURI(platformString, true);
+                return loadGenmodel(platformPluginURI.toString(), resourceSet);
+            }
+            LOG.trace("Cannot load package with URI '" + resourceOrNsURI + "'", ex);
+            return null;
+        }
+    }
 }
