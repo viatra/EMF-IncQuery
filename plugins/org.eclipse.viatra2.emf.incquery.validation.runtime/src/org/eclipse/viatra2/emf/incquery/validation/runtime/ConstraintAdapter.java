f0d749f1d4dd4f491f7d1e354f56a7a5bd180993
Tamas Szabo <szabta89@gmail.com>
2012-10-04 23:45:25 +0200

    triggerengine depdendency removed until it is added to build

--- plugins/org.eclipse.viatra2.emf.incquery.validation.runtime/src/org/eclipse/viatra2/emf/incquery/validation/runtime/ConstraintAdapter.java
+++ plugins/org.eclipse.viatra2.emf.incquery.validation.runtime/src/org/eclipse/viatra2/emf/incquery/validation/runtime/ConstraintAdapter.java
@@ -20,33 +20,33 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.viatra2.emf.incquery.runtime.api.IPatternMatch;
-import org.eclipse.viatra2.emf.incquery.triggerengine.ActivationMonitor;
-import org.eclipse.viatra2.emf.incquery.triggerengine.Agenda;
-import org.eclipse.viatra2.emf.incquery.triggerengine.Rule;
-import org.eclipse.viatra2.emf.incquery.triggerengine.TriggerEngine;
-import org.eclipse.viatra2.emf.incquery.triggerengine.firing.AutomaticFiringStrategy;
+//import org.eclipse.viatra2.emf.incquery.triggerengine.ActivationMonitor;
+//import org.eclipse.viatra2.emf.incquery.triggerengine.Agenda;
+//import org.eclipse.viatra2.emf.incquery.triggerengine.Rule;
+//import org.eclipse.viatra2.emf.incquery.triggerengine.TriggerEngine;
+//import org.eclipse.viatra2.emf.incquery.triggerengine.firing.AutomaticFiringStrategy;
 
 public class ConstraintAdapter<T extends IPatternMatch> {
 	
 	private Map<IPatternMatch, IMarker> markerMap;
-	private Agenda agenda;
+//	private Agenda agenda;
 	
 	@SuppressWarnings("unchecked")
 	public ConstraintAdapter(IEditorPart editorPart, Notifier notifier, Logger logger) {
 		this.markerMap = new HashMap<IPatternMatch, IMarker>();
 		
-		this.agenda = TriggerEngine.getInstance().createAgenda(notifier);
-		
-		for (Constraint<IPatternMatch> constraint : ValidationUtil.getConstraintsForEditorId(editorPart.getSite().getId())) {
-			Rule<IPatternMatch> rule = (Rule<IPatternMatch>) agenda.createRule(constraint.getMatcherFactory().getPattern(), true, true);
-			rule.afterAppearanceJob = new MarkerPlacerJob(markerMap, constraint, logger);
-			rule.afterDisappearanceJob = new MarkerEraserJob(markerMap, logger);
-			rule.afterModificationJob = new MarkerUpdaterJob(markerMap, constraint, logger);
-			
-			ActivationMonitor monitor = agenda.newActivationMonitor(true);
-			AutomaticFiringStrategy firingStrategy = new AutomaticFiringStrategy(monitor);
-			agenda.addCallbackAfterUpdates(firingStrategy);
-		}
+//		this.agenda = TriggerEngine.getInstance().createAgenda(notifier);
+//		
+//		for (Constraint<IPatternMatch> constraint : ValidationUtil.getConstraintsForEditorId(editorPart.getSite().getId())) {
+//			Rule<IPatternMatch> rule = (Rule<IPatternMatch>) agenda.createRule(constraint.getMatcherFactory().getPattern(), true, true);
+//			rule.afterAppearanceJob = new MarkerPlacerJob(markerMap, constraint, logger);
+//			rule.afterDisappearanceJob = new MarkerEraserJob(markerMap, logger);
+//			rule.afterModificationJob = new MarkerUpdaterJob(markerMap, constraint, logger);
+//			
+//			ActivationMonitor monitor = agenda.newActivationMonitor(true);
+//			AutomaticFiringStrategy firingStrategy = new AutomaticFiringStrategy(monitor);
+//			agenda.addCallbackAfterUpdates(firingStrategy);
+//		}
 	}
 	
 	public void dispose() {
@@ -59,6 +59,6 @@ public class ConstraintAdapter<T extends IPatternMatch> {
 			}
 		}
 		
-		agenda.dispose();
+//		agenda.dispose();
 	}
 }
