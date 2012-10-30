93e78513749e081f3cf459e1b312e0f4c96727f3
Okrosa <okrosa@gmail.com>
2012-10-30 15:54:28 +0100

    OPEN - task #347: Inconsistent Query Explorer state after closing UML graphical editor https://github.com/ujhelyiz/EMF-IncQuery/issues/issue/347

--- plugins/org.eclipse.viatra2.emf.incquery.runtime.gmf/src/org/eclipse/viatra2/emf/incquery/runtime/ui/gmf/util/GMFEditorPartListener.java
+++ plugins/org.eclipse.viatra2.emf.incquery.runtime.gmf/src/org/eclipse/viatra2/emf/incquery/runtime/ui/gmf/util/GMFEditorPartListener.java
@@ -12,6 +12,7 @@
 package org.eclipse.viatra2.emf.incquery.runtime.ui.gmf.util;
 
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.gmf.runtime.diagram.ui.parts.IDiagramWorkbenchPart;
 import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPart;
@@ -23,38 +24,42 @@ import org.eclipse.viatra2.emf.incquery.queryexplorer.util.BasePartListener;
  * The PartListener is used to observe EditorPart close actions on GMF editors.
  * 
  * @author Tamas Szabo
- *
  */
 public class GMFEditorPartListener extends BasePartListener {
-	
-	private static GMFEditorPartListener instance;
-	
-	protected GMFEditorPartListener() {
-		
-	}
-	
-	public synchronized static GMFEditorPartListener getInstance() {
-		if (instance == null) {
-			instance = new GMFEditorPartListener();
-		}
-		return instance;
-	}
 
-	@Override
-	public void partClosed(IWorkbenchPart part) {
-		if (part != null && part instanceof IEditorPart) {
-			IEditorPart closedEditor = (IEditorPart) part;
-			if (closedEditor instanceof DiagramDocumentEditor) {
-				DiagramDocumentEditor providerEditor = (DiagramDocumentEditor) closedEditor;
-				ResourceSet resourceSet = providerEditor.getEditingDomain().getResourceSet();
-				if (resourceSet.getResources().size() > 0) {
-					if (resourceSet.getResources().size() > 0) {
-						MatcherTreeViewerRootKey key = new MatcherTreeViewerRootKey(providerEditor, resourceSet);
-						QueryExplorer.getInstance().getModelConnectorMap().get(key).unloadModel();
-					}
-				}
-			}
-		}
+    private static GMFEditorPartListener instance;
 
-	}
+    protected GMFEditorPartListener() {
+
+    }
+
+    public synchronized static GMFEditorPartListener getInstance() {
+        if (instance == null) {
+            instance = new GMFEditorPartListener();
+        }
+        return instance;
+    }
+
+    @Override
+    public void partClosed(IWorkbenchPart part) {
+        if (part != null && part instanceof IEditorPart) {
+            IEditorPart closedEditor = (IEditorPart) part;
+            ResourceSet resourceSet = null;
+            if (closedEditor instanceof DiagramDocumentEditor) {
+                resourceSet = ((DiagramDocumentEditor) closedEditor).getEditingDomain().getResourceSet();
+            } else if (closedEditor instanceof IDiagramWorkbenchPart) {
+                resourceSet = ((IDiagramWorkbenchPart) closedEditor).getDiagramEditPart().getEditingDomain()
+                        .getResourceSet();
+            }
+            if (resourceSet != null && resourceSet.getResources().size() > 0) {
+                if (resourceSet.getResources().size() > 0) {
+                    MatcherTreeViewerRootKey key = new MatcherTreeViewerRootKey(closedEditor, resourceSet);
+                    if (QueryExplorer.getInstance() != null) {
+                        QueryExplorer.getInstance().getModelConnectorMap().get(key).unloadModel();
+                    }
+                }
+            }
+        }
+    }
+
 }
