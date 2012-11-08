90231b369ef3145f966904a9ead65ed1bc7f39ed
Okrosa <okrosa@gmail.com>
2012-11-08 16:06:54 +0100

    OPEN - task #352: Pattern does not show up in plug-in pattern registry https://github.com/ujhelyiz/EMF-IncQuery/issues/issue/352

---
+++ plugins/org.eclipse.viatra2.emf.incquery.runtime/src/org/eclipse/viatra2/emf/incquery/runtime/util/XmiModelUtilRunningOptionEnum.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012, Andras Okros, Istvan Rath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Andras Okros - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.viatra2.emf.incquery.runtime.util;
+
+/**
+ * An enum implementation to be used in conjuction with the XmiModelUtil.
+ */
+public enum XmiModelUtilRunningOptionEnum {
+
+    /**
+     * Use this if you intend to call the XmiModelUtil with workspace only URIs.
+     */
+    JUST_RESOURCE, 
+    
+    /**
+     * Use this if you intend to call the XmiModelUtil with plugin only URIs.
+     */
+    JUST_PLUGIN, 
+    
+    /**
+     * Default choice should be this. Use this if you intend to call the XmiModelUtil with both workspace and plugin URIs.
+     */
+    BOTH;
+    
+}
