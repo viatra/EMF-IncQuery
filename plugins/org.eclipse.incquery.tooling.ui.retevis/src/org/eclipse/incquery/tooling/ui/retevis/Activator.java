3f02cf623db759fb5efca80c50ab6a7a4f15f432
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-12-03 17:15:29 +0100

    Added header comments #359

--- plugins/org.eclipse.incquery.tooling.ui.retevis/src/org/eclipse/incquery/tooling/ui/retevis/Activator.java
+++ plugins/org.eclipse.incquery.tooling.ui.retevis/src/org/eclipse/incquery/tooling/ui/retevis/Activator.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012, Istvan Rath, Zoltan Ujhelyi and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Istvan Rath, Zoltan Ujhelyi - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.incquery.tooling.ui.retevis;
 
 import org.eclipse.jface.resource.ImageDescriptor;
