f61d9eed3b4ee7629b8d4f8f3509fc8c5dde158f
abelhegedus <hegedusa@mit.bme.hu>
2013-01-03 19:02:16 +0100

    query result associative store   Added query result map implementation   Added generic associative store for common code

--- plugins/org.eclipse.incquery.runtime.base/src/org/eclipse/incquery/runtime/base/api/QueryResultMultimap.java
+++ plugins/org.eclipse.incquery.runtime.base/src/org/eclipse/incquery/runtime/base/api/QueryResultMultimap.java
@@ -12,7 +12,6 @@ package org.eclipse.incquery.runtime.base.api;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -46,30 +45,13 @@ import com.google.common.collect.Multisets;
  * @param <ValueType>
  *            the type of the values stored in the multimap
  */
-public abstract class QueryResultMultimap<KeyType, ValueType> implements Multimap<KeyType, ValueType> {
+public abstract class QueryResultMultimap<KeyType, ValueType> extends QueryResultAssociativeStore<KeyType, ValueType> implements Multimap<KeyType, ValueType> {
 
-    /**
-     * Error literal returned when multimap modification is attempted without a setter available
-     */
-    private static final String NOT_ALLOW_MODIFICATIONS = "Query result multimap does not allow modifications";
     /**
      * This multimap contains the current key-values. Implementing classes should not modify it directly
      */
     private Multimap<KeyType, ValueType> cache;
-    /**
-     * Logger that can be used for reporting errors during runtime
-     */
-    private Logger logger;
-    /**
-     * The collection of listeners registered for this result multimap
-     */
-    private Collection<IQueryResultUpdateListener<KeyType, ValueType>> listeners;
-
-    /**
-     * The setter registered for changing the contents of the multimap
-     */
-    private IQueryResultSetter<KeyType, ValueType> setter;
-
+    
     /**
      * Constructor only visible to subclasses.
      * 
@@ -81,131 +63,46 @@ public abstract class QueryResultMultimap<KeyType, ValueType> implements Multima
         this.logger = logger;
     }
 
-    /**
-     * Registers a listener for this query result multimap that is invoked every time when a key-value pair is inserted
-     * or removed from the multimap.
-     * 
-     * <p>
-     * The listener can be unregistered via {@link #removeCallbackOnQueryResultUpdate(IQueryResultUpdateListener)}.
-     * 
-     * @param listener
-     *            the listener that will be notified of each key-value pair that is inserted or removed, starting from
-     *            now.
-     * @param fireNow
-     *            if true, notifyPut will be immediately invoked on all current key-values as a one-time effect.
+    /* (non-Javadoc)
+     * @see org.eclipse.incquery.runtime.base.api.QueryResultAssociativeStore#getCacheEntries()
      */
-    public void addCallbackOnQueryResultUpdate(IQueryResultUpdateListener<KeyType, ValueType> listener, boolean fireNow) {
-        if (listeners == null) {
-            listeners = new HashSet<IQueryResultUpdateListener<KeyType, ValueType>>();
-        }
-        listeners.add(listener);
-        if(fireNow) {
-            for (Entry<KeyType, ValueType> entry : cache.entries()) {
-                sendNotificationToListener(Direction.INSERT, entry.getKey(), entry.getValue(), listener);
-            }
-        }
+    @Override
+    protected Collection<Entry<KeyType, ValueType>> getCacheEntries() {
+        return cache.entries();
     }
 
-    /**
-     * Unregisters a callback registered by {@link #addCallbackOnQueryResultUpdate(IQueryResultUpdateListener, boolean)}
-     * .
-     * 
-     * @param listener
-     *            the listener that will no longer be notified.
+    /* (non-Javadoc)
+     * @see org.eclipse.incquery.runtime.base.api.QueryResultAssociativeStore#internalCachePut(java.lang.Object, java.lang.Object)
      */
-    public void removeCallbackOnQueryResultUpdate(IQueryResultUpdateListener<KeyType, ValueType> listener) {
-        if (listeners != null) {
-            listeners.remove(listener);
-        }
+    @Override
+    protected boolean internalCachePut(KeyType key, ValueType value) {
+        return cache.put(key, value);
     }
-
-    /**
-     * This method notifies the listeners that the query result multimap has changed.
-     * 
-     * @param direction
-     *            the type of the change (insert or delete)
-     * @param key
-     *            the key of the pair that changed
-     * @param value
-     *            the value of the pair that changed
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.incquery.runtime.base.api.QueryResultAssociativeStore#internalCacheRemove(java.lang.Object, java.lang.Object)
      */
-    private void notifyListeners(Direction direction, KeyType key, ValueType value) {
-        if(listeners != null) {
-            for (IQueryResultUpdateListener<KeyType, ValueType> listener : listeners) {
-                sendNotificationToListener(direction, key, value, listener);
-            }
-        }
+    @Override
+    protected boolean internalCacheRemove(KeyType key, ValueType value) {
+        return cache.remove(key, value);
     }
-
-    private void sendNotificationToListener(Direction direction, KeyType key, ValueType value,
-            IQueryResultUpdateListener<KeyType, ValueType> listener) throws Error {
-        try {
-            if (direction == Direction.INSERT) {
-                listener.notifyPut(key, value);
-            } else {
-                listener.notifyRemove(key, value);
-            }
-        } catch (Error e) { // NOPMD
-            throw e;
-        } catch (Throwable e) { // NOPMD
-            logger.warn(
-                    String.format(
-                            "The query result multimap encountered an error during executing a callback on %s of key %s and value %s. Error message: %s. (Developer note: %s in %s called from QueryResultMultimap)",
-                            direction == Direction.INSERT ? "insertion" : "removal", key, value, e.getMessage(), e
-                                    .getClass().getSimpleName(), listener), e);
-        }
-    }
-
-    /**
-     * Implementations of QueryResultMultimap can put a new key-value pair into the multimap with this method. If the
-     * insertion of the key-value pair results in a change, the listeners are notified.
-     * 
-     * <p>
-     * No validation or null-checking is performed during the method!
-     * 
-     * @param key
-     *            the key which identifies the collection where the new value is put
-     * @param value
-     *            the value that is put into the collection of the key
-     * @return true, if the insertion resulted in a change (the key-value pair was not yet in the multimap)
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.incquery.runtime.base.api.QueryResultAssociativeStore#internalCacheContainsEntry(java.lang.Object, java.lang.Object)
      */
-    protected boolean internalPut(KeyType key, ValueType value) {
-        boolean putResult = cache.put(key, value);
-        if (putResult) {
-            notifyListeners(Direction.INSERT, key, value);
-        }
-        return putResult;
+    @Override
+    protected boolean internalCacheContainsEntry(KeyType key, ValueType value) {
+        return cache.containsEntry(key, value);
     }
-
-    /**
-     * Implementations of QueryResultMultimap can remove a key-value pair from the multimap with this method. If the
-     * removal of the key-value pair results in a change, the listeners are notified.
-     * 
-     * <p>
-     * No validation or null-checking is performed during the method!
-     * 
-     * @param key
-     *            the key which identifies the collection where the value is removed from
-     * @param value
-     *            the value that is removed from the collection of the key
-     * @return true, if the removal resulted in a change (the key-value pair was in the multimap)
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.incquery.runtime.base.api.QueryResultAssociativeStore#internalCacheSize()
      */
-    protected boolean internalRemove(KeyType key, ValueType value) {
-        boolean removeResult = cache.remove(key, value);
-        if (removeResult) {
-            notifyListeners(Direction.DELETE, key, value);
-        }
-        return removeResult;
+    @Override
+    protected int internalCacheSize() {
+        return cache.size();
     }
-
-    /**
-     * @param setter
-     *            the setter to set
-     */
-    public void setQueryResultSetter(IQueryResultSetter<KeyType, ValueType> setter) {
-        this.setter = setter;
-    }
-
+    
     /**
      * @return the cache
      */
@@ -219,13 +116,6 @@ public abstract class QueryResultMultimap<KeyType, ValueType> implements Multima
      */
     protected void setCache(Multimap<KeyType, ValueType> cache) {
         this.cache = cache;
-    }
-
-    /**
-     * @return the logger
-     */
-    protected Logger getLogger() {
-        return logger;
     }
 
     // ======================= implemented Multimap methods ======================
@@ -337,7 +227,7 @@ public abstract class QueryResultMultimap<KeyType, ValueType> implements Multima
      */
     @Override
     public Collection<Entry<KeyType, ValueType>> entries() {
-        return Collections.unmodifiableCollection(cache.entries());
+        return Collections.unmodifiableCollection(getCacheEntries());
     }
 
     /**
@@ -364,87 +254,6 @@ public abstract class QueryResultMultimap<KeyType, ValueType> implements Multima
             throw new UnsupportedOperationException(NOT_ALLOW_MODIFICATIONS);
         }
         return modifyThroughQueryResultSetter(key, value, Direction.INSERT);
-    }
-
-    /**
-     * This method is used for calling the query result setter to put or remove a value by modifying the model.
-     * 
-     * <p>
-     * The given key-value pair is first validated (see {@link IQueryResultSetter#validate(Object, Object)}, then the
-     * put or remove method is called (see {@link IQueryResultSetter#put(Object, Object)} and
-     * {@link IQueryResultSetter#remove(Object, Object)}). If the setter reported that the model has been changed, the
-     * change is checked.
-     * 
-     * <p>
-     * If the model modification did not change the result set in the desired way, a warning is logged.
-     * 
-     * <p>
-     * If the setter throws any {@link Throwable}, it is either rethrown in case of {@link Error} and logged otherwise.
-     * 
-     * 
-     * @param key
-     *            the key of the pair to be inserted or removed
-     * @param value
-     *            the value of the pair to be inserted or removed
-     * @param direction
-     *            specifies whether a put or a remove is performed
-     * @return true, if the multimap changed according to the direction
-     */
-    private boolean modifyThroughQueryResultSetter(KeyType key, ValueType value, Direction direction) {
-        try {
-            if (setter.validate(key, value)) {
-                final int size = cache.size();
-                final int expectedChange = (direction == Direction.INSERT) ? 1 : -1;
-                boolean changed = false;
-                if (direction == Direction.INSERT) {
-                    changed = setter.put(key, value);
-                } else {
-                    changed = setter.remove(key, value);
-                }
-                if (changed) {
-                    return checkModificationThroughQueryResultSetter(key, value, direction, expectedChange, size);
-                } else {
-                    logger.warn(String
-                            .format("The query result multimap %s of key %s and value %s resulted in %s. (Developer note: %s called from QueryResultMultimap)",
-                                    direction == Direction.INSERT ? "insertion" : "removal", key, value,
-                                    Math.abs(cache.size() - size) > 1 ? "more than one changed result"
-                                            : "no changed results", setter));
-                }
-            }
-        } catch (Error e) { // NOPMD
-            throw e;
-        } catch (Throwable e) { // NOPMD
-            logger.warn(
-                    String.format(
-                            "The query result multimap encountered an error during invoking setter on %s of key %s and value %s. Error message: %s. (Developer note: %s in %s called from QueryResultMultimap)",
-                            direction == Direction.INSERT ? "insertion" : "removal", key, value, e.getMessage(), e
-                                    .getClass().getSimpleName(), setter), e);
-        }
-
-        return false;
-    }
-
-    /**
-     * Checks whether the model modification performed by the {@link IQueryResultSetter} resulted in the insertion or
-     * removal of exactly the required key-value pair.
-     * 
-     * @param key
-     *            the key for the pair that was inserted or removed
-     * @param value
-     *            the value for the pair that was inserted or removed
-     * @param direction
-     *            the direction of the change
-     * @param size
-     *            the size of the cache before the change
-     * @return true, if the changes made by the query result setter were correct
-     */
-    private boolean checkModificationThroughQueryResultSetter(KeyType key, ValueType value, Direction direction,
-            final int expectedChange, final int size) {
-        if ((direction == Direction.INSERT) == cache.containsEntry(key, value)
-                && (cache.size() - expectedChange) == size) {
-            return true;
-        }
-        return false;
     }
 
     /**
@@ -575,27 +384,7 @@ public abstract class QueryResultMultimap<KeyType, ValueType> implements Multima
      */
     @Override
     public void clear() {
-        if (setter == null) {
-            throw new UnsupportedOperationException(NOT_ALLOW_MODIFICATIONS);
-        }
-        Collection<Entry<KeyType, ValueType>> entries = Lists.newArrayList(cache.entries());
-        Iterator<Entry<KeyType, ValueType>> iterator = entries.iterator();
-        while (iterator.hasNext()) {
-            Entry<KeyType, ValueType> entry = iterator.next();
-            modifyThroughQueryResultSetter(entry.getKey(), entry.getValue(), Direction.DELETE);
-        }
-        if (cache.size() != 0) {
-            StringBuilder sb = new StringBuilder();
-            for (Entry<KeyType, ValueType> entry : cache.entries()) {
-                if (sb.length() > 0) {
-                    sb.append(", ");
-                }
-                sb.append(entry.toString());
-            }
-            logger.warn(String
-                    .format("The query result multimap is not empty after clear, remaining entries: %s. (Developer note: %s called from QueryResultMultimap)",
-                            sb.toString(), setter));
-        }
+        internalClear();
     }
 
 }
